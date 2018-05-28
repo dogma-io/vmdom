3ffbc8693c2aa2e2fa64b79569f663a774ab5447
Matthew Dahl <dahl.matthew@gmail.com>
2018-05-28 13:03:53 -0700

    Upgrade dev deps

--- .babelrc.js
+++ .babelrc.js
@@ -1,18 +1,3 @@
 module.exports = {
-  plugins: [
-    '@babel/plugin-proposal-class-properties',
-    '@babel/plugin-syntax-object-rest-spread',
-  ],
-  presets: [
-    [
-      '@babel/env',
-      {
-        targets: {
-          node: '6',
-        },
-      },
-    ],
-    '@babel/flow',
-    '@babel/react',
-  ],
+  presets: ['babel-preset-nodely'],
 }
